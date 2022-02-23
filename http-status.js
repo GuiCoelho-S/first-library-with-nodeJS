70e48cb04e69bb68e8d95702eb7a31ae4ed49437
GuiCoelho-S <gs.coelho@outlook.com>
2022-02-23 20:10:40 -0300

    criando ambiente test - jest

--- http-status.js
+++ http-status.js
@@ -1,25 +1,42 @@
 import chalk from "chalk";
 import fetch from "node-fetch";
 
+function handleErrors(err){
+  throw new Error(err.message);
+}
+ 
 async function checkstatus(urlArrays){
   
-  const statusArray = await Promise.all(urlArrays.map(async url => {
-    console.log(chalk.greenBright(url))
-    const res = await fetch(url);
-    return res.status
+  try {
+    const statusArray = await Promise
+    .all(urlArrays
+      .map(async url => {
+        console.log(chalk.greenBright(url))
+        const res = await fetch(url);
+        return `${res.status} - ${res.statusText}`;
   }))
-  return statusArray
+      return statusArray
+
+  } catch(err){
+      handleErrors(err);
+  }
 }
 
 function getUrlArray(linksArray){
-  return linksArray.map(LinkObj => Object.values(LinkObj).join());
+  return linksArray
+    .map(LinkObj => Object
+      .values(LinkObj).join());
 }
 
 async function validateUrl(linksArray){
   const links = getUrlArray(linksArray)
   const statusLinks = await checkstatus(links);
-
-  return statusLinks
+  
+  const results = linksArray.map((obj, index) => ({
+    ...obj, 
+    status: statusLinks[index]
+  }));
+  return results
 }
 
 export { validateUrl }
---
+++ test/index.test.js
@@ -0,0 +1,6 @@
+const getArchive = require('../index')
+
+test('deve ser uma função', () => {
+
+  expect(typeof getArchive).toBe('function')
+});
